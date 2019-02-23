dbef3382ab37ae7e828f149cb587920e7fe13754
Bert Haddad <bhaddad5@gmail.com>
2019-02-23 12:04:39 -0800

    Unity example working!

---
+++ NameGenerator/Name Generator Example/Assets/Name Generator/NameGeneratorComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NameGenerator;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameGeneratorComponent : MonoBehaviour
+{
+	public Dropdown Dropdown;
+	public Text DisplayText;
+
+	public void GenerateName()
+	{
+		NameRequestHandler gen = new NameRequestHandler();
+		string path = Path.Combine(Application.streamingAssetsPath, Path.Combine("Name Generator/Name Tables/", Dropdown.options[Dropdown.value].text + ".txt"));
+		DisplayText.text = gen.HandleNameRequest(path, 1).FirstOrDefault();
+	}
+}
